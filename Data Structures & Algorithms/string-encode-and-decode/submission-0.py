8c34a89221cf5976cb359e41902a90d0f88e88ee
Janhavi Belnekar <114862128+Janhavi30052002@users.noreply.github.com>
2026-06-22 18:08:54 +0530

    Bulk sync: 15 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+
+            # Find the delimiter "#"
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+
+            # Extract the string
+            res.append(s[j + 1 : j + 1 + length])
+
+            # Move pointer
+            i = j + 1 + length
+
+        return res
